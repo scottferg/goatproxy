efbf7ba9af00be2c6e2e1f80d8f9fd6a34c6eda0
Scott Ferguson <scottwferg@gmail.com>
2014-06-10 08:56:27 -0500

    Rewrite

--- colors.go
+++ colors.go
@@ -1,115 +1,49 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
 )
 
 func colored(text, color string) string {
 	return fmt.Sprintf("\033[%sm%s\033[0m", color, text)
 }
 
-func yellow(text string) string {
+func Yellow(text string) string {
 	return colored(text, "33")
 }
 
-func yellowBold(text string) string {
+func YellowBold(text string) string {
 	return colored(text, "1;33")
 }
 
-func green(text string) string {
+func Green(text string) string {
 	return colored(text, "32")
 }
 
-func blue(text string) string {
+func GreenBold(text string) string {
+	return colored(text, "1;32")
+}
+
+func Blue(text string) string {
 	return colored(text, "34")
 }
 
-func blueBold(text string) string {
+func BlueBold(text string) string {
 	return colored(text, "1;34")
 }
 
-func red(text string) string {
+func Red(text string) string {
 	return colored(text, "31")
 }
 
-func redBold(text string) string {
+func RedBold(text string) string {
 	return colored(text, "1;31")
 }
 
-func cyan(text string) string {
+func Cyan(text string) string {
 	return colored(text, "36")
 }
 
-func cyanBold(text string) string {
+func CyanBold(text string) string {
 	return colored(text, "1;36")
-}
-
-func renderBody(r *http.Response, buf *bytes.Buffer) {
-	buf.Write([]byte("\r\n"))
-
-	var objmap interface{}
-	body, _ := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-
-	contentType := strings.Split(r.Header["Content-Type"][0], ";")
-	switch contentType[0] {
-	case "application/json":
-		_ = json.Unmarshal(body, &objmap)
-
-		res, _ := json.MarshalIndent(objmap, "", "    ")
-		buf.Write(res)
-	case "text/html":
-		buf.Write(body)
-	case "application/xml":
-		_ = xml.Unmarshal(body, &objmap)
-
-		res, _ := xml.MarshalIndent(objmap, "", "    ")
-		buf.Write(res)
-	}
-
-	buf.Write([]byte("\r\n"))
-}
-
-func formattedRequest(r *http.Request) string {
-	buf := new(bytes.Buffer)
-
-	buf.Write([]byte(fmt.Sprintf("%s %s %s\n", green(r.Method), r.URL.String(), r.Proto)))
-	buf.Write([]byte(fmt.Sprintf("%s: %s\n", green("Host"), r.Host)))
-
-	if r.Header != nil {
-		for k, v := range r.Header {
-			buf.Write([]byte(fmt.Sprintf("%s: %s\n", green(k), v[0])))
-		}
-	}
-
-	return string(buf.Bytes())
-}
-
-func formattedResponse(r *http.Response) string {
-	buf := new(bytes.Buffer)
-
-	switch {
-	case r.StatusCode >= 500 && r.StatusCode < 600:
-		buf.Write([]byte(fmt.Sprintf("%s %s\n", r.Proto, redBold(r.Status))))
-	default:
-		buf.Write([]byte(fmt.Sprintf("%s %s\n", r.Proto, cyanBold(r.Status))))
-	}
-
-	if r.Header != nil {
-		for k, v := range r.Header {
-			buf.Write([]byte(fmt.Sprintf("%s: %s\n", green(k), v[0])))
-		}
-	}
-
-	if r.Header["Content-Type"] != nil && !*with_ui {
-		renderBody(r, buf)
-	}
-
-	return string(buf.Bytes())
 }
---
+++ main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	host    *string = flag.String("host", "", "target host or address")
+	port    *string = flag.String("port", ":8080", "target port")
+	latency *int    = flag.Int("latency", 0, "in milliseconds")
+)
+
+type errorHandler func(w http.ResponseWriter, r *http.Request) error
+
+func (e errorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := e(w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func prettyPrintBody(body *bytes.Buffer) ([]byte, error) {
+	buf := &bytes.Buffer{}
+
+	if len(body.Bytes()) > 0 {
+		err := json.Indent(buf, body.Bytes(), "", "    ")
+		if err != nil {
+			return nil, err
+		}
+
+		return buf.Bytes(), nil
+	}
+
+	return nil, nil
+}
+
+func prettyPrintRequest(r *http.Request, content string, body *bytes.Buffer) error {
+	fmt.Printf("%s %s %s\n", GreenBold(r.Method), r.URL.String(), Blue(r.Proto))
+
+	for header, values := range r.Header {
+		fmt.Printf("%s: %s\n", YellowBold(header), strings.Join(values, ", "))
+	}
+
+	var output []byte
+	var err error
+
+	switch content {
+	case "application/json":
+		output, err = prettyPrintBody(body)
+		if err != nil {
+			return err
+		}
+	default:
+		output = body.Bytes()
+	}
+
+	if len(output) > 0 {
+		fmt.Println(string(output))
+	}
+
+	fmt.Printf("\n\n")
+	return nil
+}
+
+func prettyPrintResponse(r *http.Response, content string, body *bytes.Buffer) error {
+	fmt.Printf("%s %s\n", GreenBold(r.Proto), Blue(r.Status))
+
+	for header, values := range r.Header {
+		fmt.Printf("%s: %s\n", YellowBold(header), strings.Join(values, ", "))
+	}
+
+	var output []byte
+	var err error
+
+	switch content {
+	case "application/json":
+		output, err = prettyPrintBody(body)
+		if err != nil {
+			return err
+		}
+	default:
+		output = body.Bytes()
+	}
+
+	fmt.Printf("%s\n\n\n", string(output))
+	return nil
+}
+
+func proxyHandler(w http.ResponseWriter, r *http.Request) error {
+	buf := new(bytes.Buffer)
+	reader := io.TeeReader(r.Body, buf)
+	defer r.Body.Close()
+
+	// Write new request to dest
+	var scheme string
+	switch r.URL.Scheme {
+	case "":
+		scheme = "http://"
+	default:
+		scheme = r.URL.Scheme
+	}
+
+	req, err := http.NewRequest(r.Method, fmt.Sprintf("%s%s%s", scheme, *host, r.URL.String()), reader)
+	if err != nil {
+		return err
+	}
+	req.Header = r.Header
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	// Simulate latency
+	time.Sleep(time.Duration(*latency) * time.Millisecond)
+
+	err = prettyPrintRequest(r, resp.Header["Content-Type"][0], buf)
+	if err != nil {
+		return err
+	}
+
+	buf = bytes.NewBuffer(nil)
+	reader = io.TeeReader(resp.Body, buf)
+	defer resp.Body.Close()
+
+	for header, values := range resp.Header {
+		w.Header().Set(header, strings.Join(values, ", "))
+	}
+
+	_, err = bufio.NewWriter(w).ReadFrom(reader)
+
+	switch {
+	case len(resp.Header["Content-Encoding"]) > 0 && resp.Header["Content-Encoding"][0] == "gzip":
+		uncom, err := gzip.NewReader(buf)
+		if err != nil {
+			return err
+		}
+
+		buf = bytes.NewBuffer(nil)
+		_, err = buf.ReadFrom(uncom)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = prettyPrintResponse(resp, resp.Header["Content-Type"][0], buf)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func init() {
+	flag.Parse()
+
+	if flag.NFlag() < 1 {
+		fmt.Println("usage: netproxy -host target_host")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+}
+
+func main() {
+	http.Handle("/", errorHandler(proxyHandler))
+
+	log.Fatal(http.ListenAndServe(*port, nil))
+}
